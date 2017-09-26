7db9731d9e0d87289c9a07436c03dc20ec943ffa
bugfr <bugfroggy@gmail.com>
2017-09-25 23:48:24 -0400

    Renamed mod from Advanced Experience to Advanced XP

--- src/main/java/co/bugg/advancedxp/Reference.java
+++ src/main/java/co/bugg/advancedxp/Reference.java
@@ -2,7 +2,7 @@ package co.bugg.advancedxp;
 
 public class Reference {
     public static final String MOD_ID = "advancedxp";
-    public static final String MOD_NAME = "Advanced Experience";
+    public static final String MOD_NAME = "Advanced XP";
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERSIONS = "[1.8.9]";
 }
