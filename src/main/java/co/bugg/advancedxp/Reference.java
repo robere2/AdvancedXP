91f0782fde2a698bc947981903d5a0380ad87608
bugfr <bugfroggy@gmail.com>
2017-09-26 00:45:34 -0400

    Fixed typo in accepted versions

--- src/main/java/co/bugg/advancedxp/Reference.java
+++ src/main/java/co/bugg/advancedxp/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
     public static final String MOD_ID = "advancedxp";
     public static final String MOD_NAME = "Advanced XP";
     public static final String VERSION = "1.0";
-    public static final String ACCEPTED_VERSIONS = "[1.12-1.12.2]";
+    public static final String ACCEPTED_VERSIONS = "[1.12,1.12.2]";
 }
