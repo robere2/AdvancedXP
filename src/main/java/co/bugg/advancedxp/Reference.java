9c9a37801b7bc367b5df0f19ae9734e42ef20232
bugfr <bugfroggy@gmail.com>
2017-09-27 21:24:12 -0400

    added versions.json, fixed null pointer bug

--- src/main/java/co/bugg/advancedxp/Reference.java
+++ src/main/java/co/bugg/advancedxp/Reference.java
@@ -3,6 +3,6 @@ package co.bugg.advancedxp;
 public class Reference {
     public static final String MOD_ID = "advancedxp";
     public static final String MOD_NAME = "Advanced XP";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     public static final String ACCEPTED_VERSIONS = "[1.12,1.12.2]";
 }
